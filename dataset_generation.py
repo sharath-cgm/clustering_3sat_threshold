9365ae24a65f3edbb01a020146f859dfa02c9edd
sharath-cgm <gurukulamitra@gmail.com>
2022-08-04 11:54:29 -0400

    added labels

--- dataset_generation.py
+++ dataset_generation.py
@@ -1,47 +1,38 @@
 
-
-import random
-from re import U
-
 import numpy as np
 
 #Variables - Clustering
 N = 60   #edges = n*dl = m*dr = N
-K = 4    #num of clusters ---> n = k 
+k = 4    #num of clusters ---> n = k 
 #Variables - Bipartite Graph
-dl =15   #degree left side
+dl = 15   #degree left side
 dr = 2   #degree right side
 n = 4    #left side
 m = 30   #right side
 
 
-data = np.zeros((N, n+m))
-
+data = np.zeros((N, n+m+1)) # last column for labels
 
 right_deg = np.array([dr]*m) 
 left_deg = np.array([dl]*n) 
 
-print("Left Degree:", left_deg)
-print("Right Degree:", right_deg)
+# print("Left Degree:", left_deg)
+# print("Right Degree:", right_deg)
 
 
 for i in range(N): 
+    y = np.random.choice(n, p=left_deg / np.sum(left_deg)) # random vertex from left set
+    x = np.random.choice(m, p=right_deg / np.sum(right_deg)) # random vertex from right set
+    # print("y, x+N: ", y,x+m)
     
-
-    x = np.random.choice(m, p=right_deg / np.sum(right_deg))
-    
-   # print("y, x+N: ", y,x+m)
-
-    y = np.random.choice(n, p=left_deg / np.sum(left_deg))
-    
-
     data[i][x+n] = 1
     data[i][y] = 1
+    data[i][-1] = y
 
     right_deg[x] -= 1
     left_deg[y] -= 1
     
-    
+
 """"   
 row = np.array([N*d])
 data = np.append(right_deg,[row],axis=0)
@@ -50,10 +41,11 @@ col = np.array([Nd*2*N+1])
 data = np.append(left_deg,[col],axis=0)
 """
 
-filename = "Dataset"
+# print("Dataset:", data)
+
+filename = "dataset"
 np.savetxt(filename + ".txt", data, fmt = '%u', delimiter=" ")
 
  
-print("Dataset:", data)
 
 
